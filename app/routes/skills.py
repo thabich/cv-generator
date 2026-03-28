125b1cbcb31ce672c46cd9019ad93d1d4f812a71
Thorsten Habich <thorsten.habich@findichgut.net>
2026-03-28 19:54:47 +0200

    save point

--- app/routes/skills.py
+++ app/routes/skills.py
@@ -2,19 +2,22 @@ from flask import Blueprint, render_template, request, redirect, url_for, jsonif
 from ..utils import load_json, save_json, DATA_FILES
 
 skills_bp = Blueprint("skills_bp", __name__)
+def sort_skills(skills):
+    return sorted(skills, key=lambda k: k['name'], reverse=False)
 
 @skills_bp.route("/", methods=["GET","POST"])
 def skills_view():
     skills = load_json(DATA_FILES["skills"])
+    skills_sorted = sort_skills(skills)
     categories = load_json(DATA_FILES["categories"])
     if request.method == "POST":
         name = request.form.get("name")
         name_de = request.form.get("name_de") or ""
         category = request.form.get("category") or ""
         skills.append({"name": name, "name_de": name_de, "category": category})
-        save_json(DATA_FILES["skills"], skills)
+        save_json(DATA_FILES["skills"], sort_skills(skills))
         return redirect(url_for("skills_bp.skills_view"))
-    return render_template("skills.html", skills=skills, categories=categories)
+    return render_template("skills.html", skills=sort_skills(skills), categories=categories)
 
 @skills_bp.route("/edit", methods=["POST"])
 def edit_skill():
@@ -24,7 +27,7 @@ def edit_skill():
     skills[index]["name"] = data["name"]
     skills[index]["name_de"] = data.get("name_de","")
     skills[index]["category"] = data.get("category","")
-    save_json(DATA_FILES["skills"], skills)
+    save_json(DATA_FILES["skills"], sort_skills(skills))
     return '', 204
 
 @skills_bp.route("/delete", methods=["POST"])
@@ -33,5 +36,5 @@ def delete_skill():
     index = int(data["index"])
     skills = load_json(DATA_FILES["skills"])
     skills.pop(index)
-    save_json(DATA_FILES["skills"], skills)
+    save_json(DATA_FILES["skills"], sort_skills(skills))
     return '', 204
